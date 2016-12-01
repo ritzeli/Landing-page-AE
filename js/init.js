113626e323ef81a806e0bb45fc55343b57855f53
memo <ingepulido@hotmail.com>
2016-12-01 13:51:24 +1000

    Iniciamos

---
+++ js/init.js
@@ -0,0 +1,26 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+
+  }); // end of document ready
+   $('.modal-trigger').leanModal({
+      dismissible: true, // Modal can be dismissed by clicking outside of the modal
+      opacity: .5, // Opacity of modal background
+      in_duration: 300, // Transition in duration
+      out_duration: 200, // Transition out duration
+      starting_top: '4%', // Starting top style attribute
+      ending_top: '10%', // Ending top style attribute
+    }
+  );
+
+   
+    $('.slider').slider({
+      	full_width: true,
+      	indicators:false,
+    }
+
+  );    
+
+})(jQuery); 
+
